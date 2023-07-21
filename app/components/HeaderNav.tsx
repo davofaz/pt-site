9ef1beb1cf0fe4a023c9cf1bf0b31905d821e815
davofaz <dfazaldin@gmail.com>
2023-07-21 17:34:00 +0100

    style: fixed some visual issues with the header/nav

--- app/components/HeaderNav.tsx
+++ app/components/HeaderNav.tsx
@@ -16,60 +16,55 @@ const Navigation = () => {
 
     // Calculate the reduced font size in rem units
     const h1 = "1.125rem";
-    const h1smaller = `${parseInt(h1) * 0.9}rem`; // Reduce by 10% (0.9)
-    const h2 = "0.9rem";
-    const h2smaller = `${parseInt(h2) - 0.1}rem`; // Reduce by 10% (0.9)
-    const logoWidth = 64;
-    const logoWidthSmaller = 48;
-
+    const h1smaller = `${parseInt(h1) * 0.95}rem`; // Reduce by 10% (1)
+    const h2 = "1rem";
+    const h2smaller = `${parseInt(h2) - 0.2}rem`; // Reduce by -2 (0.7)
+    const logoWidth = 48;
+    const logoWidthSmaller = 42;
+    const opacity = "0.95";
+    const shadow = "rgba(0, 0, 0, 0.45) 0px 25px 20px -20px";
 
     // Calculate the class name based on scroll position
     const h1FontClass = clientWindowHeight > 0 ? h1smaller : h1;
     const h2FontClass = clientWindowHeight > 0 ? h2smaller : h2;
-        
-    const logoWidthClass = clientWindowHeight > 0 ? logoWidthSmaller : logoWidth;
-
-    const opacity = "0.95";
-    const opacityClass = clientWindowHeight > 0 ? opacity : 1;
-
-    const shadow = "rgba(0, 0, 0, 0.45) 0px 25px 20px -20px";
+    const logoWidthClass = clientWindowHeight > 0 ? logoWidthSmaller : logoWidth;    
+    const opacityClass = clientWindowHeight > 0 ? opacity : 1;      
     const shadowClass = clientWindowHeight > 0 ? shadow : '';    
-   // console.log('shadow: ', shadowClass)
+   // console.log('image: ', logoWidthClass)
 
     return (
         <div className="sticky bg-white -top-1" style={{ boxShadow: shadowClass, opacity: opacityClass }}>
-        <div className=" shadow-gray-400 flex flex-row items-center pt-3 pb-1">
-          <a href="/">
-            <Image
-                className="flex-none mr-3"
-                src="/logo-512.png"
-                alt="Luton International Church"
-                width={logoWidthClass}
-                height={64}
-                style={{ width: "100%", height: "auto" }}
-                priority={true}
+            <div className=" shadow-gray-400 flex flex-row items-center pt-3 pb-4 border-b border-violet-800">
+              <a href="/">
+                <Image
+                    src="/logo-512.png"
+                    alt="Luton International Church"
+                    width={logoWidthClass}
+                    height={48}
+                    priority={true}
+                    //style={{ width: "auto", height: "auto" }}
 
-            />
-          </a>
-            <ul className="flex flex-col">
-            <li>
-              <h1 className="text-lg md:text-4xl capitalize font-bold text-violet-900 font-sans flex-grow"
-                style={{ fontSize: h1FontClass }}
-              >
-                Luton International Church
-              </h1>
-            </li>
-            <li className="flex">
-              <h2
-                className="text-sm capitalize -mt-2 font-light text-violet-900 font-sans mb-3"
-                style={{ fontSize: h2FontClass }}
-              >
-                Enlarging the tent to inherit the Nations
-              </h2>
-            </li>
-          </ul>
+                />
+              </a>
+                <ul className="flex flex-col ml-3">
+                <li>
+                  <h1 className="text-lg capitalize font-bold text-violet-900 font-sans"
+                    style={{ fontSize: h1FontClass }}
+                  >
+                    Luton International Church
+                  </h1>
+                </li>
+                <li className="flex">
+                  <h2
+                    className="text-sm capitalize font-light text-violet-900 font-sans"
+                    style={{ fontSize: h2FontClass }}
+                  >
+                    Enlarging the tent to inherit the Nations
+                  </h2>
+                </li>
+              </ul>
             </div>
-            <nav className="pb-2 ml-2">
+            <nav className="pb-2 ml-1 mt-2">
                 <ul className="flex flex-row space-x-4 text-violet-900">
                     <li><a className="border-b-4 border-white hover:border-violet-900 active:border-violet-600" href="#about">About</a></li>
                     <li><a className="border-b-4 border-white hover:border-violet-900 active:border-violet-600" href="#outreach">Outreach</a></li>
