34aeccbcb71b8698dcacc0d950380f8699d5ba1e
davofaz <dfazaldin@gmail.com>
2023-07-31 13:08:28 +0100

    feature: added og:image to Head

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,8 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'EMC Personal Training & Wellness Coaching',
   description: 'Fully Equipped Private Training Facility in Dorset - 1 to 1 Personal Training and Coaching',
+  openGraphImage: '/pt-logo-512-3.png'
+
 }
 
 export default function RootLayout({
@@ -21,6 +23,7 @@ export default function RootLayout({
         <title>{metadata.title as string}</title>
         <meta name="description" content={metadata.description as string} />
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:image" content={metadata.openGraphImage} />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
